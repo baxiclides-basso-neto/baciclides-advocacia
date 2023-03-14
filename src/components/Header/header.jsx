8cda7c7f6496008c6a8364e377e4f3f5f4b94b9d
NetoBasso <baxnetodev@gmail.com>
2023-03-14 13:11:28 -0300

    Esling installation and Header Component and stylization add

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { HeaderContainer, Logo, Nav } from './style';
+
+function Header() {
+    return (
+        
+      <HeaderContainer>
+        <Logo>Nome do escritório de advocacia</Logo>
+        <Nav>
+          <ul>
+            <li><a href="#">Página inicial</a></li>
+            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+        </Nav>
+      </HeaderContainer>
+    );
+  }
+  
+  export default Header;
---
+++ src/components/Header/style.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  background-color: #FFFFFF;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const Logo = styled.h1`
+  font-size: 24px;
+  color: #333333;
+`;
+
+export const Nav = styled.nav`
+  ul {
+    display: flex;
+    list-style: none;
+  }
+
+  li {
+    margin-left: 24px;
+  }
+
+  a {
+    font-size: 18px;
+    color: #333333;
+    text-decoration: none;
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+      color: #0088CC;
+    }
+  }
+`;
